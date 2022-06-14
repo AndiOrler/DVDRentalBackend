4461b25ff0129d07144f07da67f82b2eca73529a
AndiOrler <andreas.orler@gmail.com>
2022-06-14 21:31:27 +0200

    test refactor

--- src/Services/MyService.cs
+++ src/Services/MyService.cs
@@ -5,7 +5,7 @@ public class MyService
 {
     public IEnumerable<WeatherForecast> GetString(String[] strings)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, strings.Length).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
